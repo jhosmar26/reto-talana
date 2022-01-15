3ea3119d9c3753874ecddf2c4058523ba0cbe78f
Jhose Portillo <jhosmar26.00@gmail.com>
2022-01-15 12:15:25 -0500

    ADD: content card component

---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="w-60 h-80 flex justify-center items-center">
+      <img class="max-w-full max-h-full" :src="element.photo" alt="">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  props:["element"]
+}
+</script>
---
+++ src/components/Content.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="">
+    <div>Categoría</div>
+    <div>breadcrumb</div>
+    <div class="flex flex-wrap">
+      <div v-for="product,key in productsToShow" :key="key">
+        <Card :element="product"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from "./Card.vue"
+
+export default {
+  data() {
+    return{
+      products: [],
+      productsToShow: [],
+      paginatorIndex: 0,
+    }
+  },
+  components:{
+    Card
+  },
+  mounted(){
+    this.getProducts()
+  },
+  methods:{
+    getProducts(){
+      this.axios
+        .get("http://sva.talana.com:8000/api/product/")
+        .then((response)=>{
+          this.products = response.data;
+          this.productsToShow = response.data;
+        })
+    },
+  }
+}
+</script>
