2011d1266828f15d5fea26a6420a310d32c0e49c
Jhose Portillo <jhosmar26.00@gmail.com>
2022-01-15 12:15:03 -0500

    EDIT: grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="grid-custom-cols bg-[#fff000]">
+      <div class="text-3xl text-center self-center">Logo</div>
+      <div class=""><Nav/></div>
+      <div><Sidebar/></div>
+      <div><Content/></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from "./components/Nav.vue";
+import Sidebar from "./components/Sidebar.vue";
+import Content from "./components/Content.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
+    Sidebar,
+    Content
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.grid-custom-cols{
+  display: grid;
+  grid-template-columns: auto 1fr;
 }
 </style>
