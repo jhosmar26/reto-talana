5bf754ae90abf4192be86cf1a152a671faef03d1
Jhose Portillo <jhosmar26.00@gmail.com>
2022-01-15 11:20:55 -0500

    ADD: axios

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
 require("@/index.css");
 
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
 new Vue({
   render: (h) => h(App),
+  axios,
 }).$mount("#app");
